5f859c38506f330a2227907528975e5078dde00c
Diya050 <13diyabaweja11a@gmail.com>
2023-09-15 13:15:57 +0530

    Function for sending end meeting message

--- meeting/api.py
+++ meeting/api.py
@@ -42,7 +42,7 @@ def end_meeting_message(meeting):
     meeting = frappe.get_doc("Meeting", meeting)
 
     if meeting.status == "In Progress":
-        """message = "Meeting has ended."
+        message = "Meeting has ended."
 
         for attendee in meeting.attendees:
             frappe.publish_realtime(event="meeting_ended", message=message, user=attendee.attendee)
@@ -54,12 +54,12 @@ def end_meeting_message(meeting):
                 subject="Meeting Ended: " + meeting.title,
                 message=message
             )
-"""
+
         meeting.status = "Completed"
         meeting.save()
         frappe.msgprint(_("Meeting Status updated to 'Completed'"))
     else:
-        frappe.msgprint(_("Meeting Status must be 'In Progress' to start the meeting"))
+        frappe.msgprint(_("Meeting Status must be 'In Progress' to end the meeting"))
 
 	
 
