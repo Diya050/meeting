8b6d182457945852bfce0de05d37974899386455
Diya050 <13diyabaweja11a@gmail.com>
2023-11-18 20:46:08 +0530

    Rectifying exception erroe

--- meeting/api.py
+++ meeting/api.py
@@ -106,7 +106,8 @@ def start_meeting_message(meeting):
 
     if meeting.status == "Invitation Sent":
         current_time = now_datetime()
-        meeting_from_time = get_datetime(str(meeting.date) + ' ' + str(meeting.from_time))  # Convert to str
+        meeting_from_time = get_datetime(str(meeting.date) + ' ' + str(meeting.from_time + ':00'))
+
 
         if current_time > meeting_from_time:
             delay_seconds = (current_time - meeting_from_time).seconds
@@ -158,14 +159,14 @@ def get_meetings(start, end):
 		"end": end
 	}, as_dict=True)
 
-def make_orientation_meeting(doc, method):
-	"""Create an orientation meeting when a new User is added"""
+"""def make_orientation_meeting(doc, method):
+	//Create an orientation meeting when a new User is added
 	meeting = frappe.get_doc({
 		"doctype": "Meeting",
 		"title": "Orientation for {0}".format(doc.first_name),
 		"date": add_days(nowdate(), 1),
-		"from_time": "09:00",
-		"to_time": "09:30",
+		"from_time": "09:00:00",
+		"to_time": "09:30:00",
 		"status": "Planned",
 		"attendees": [{
 			"attendee": doc.name
@@ -178,7 +179,7 @@ def make_orientation_meeting(doc, method):
 	frappe.msgprint(_("Orientation meeting created"))
 
 def update_minute_status(doc, method=None):
-	"""Update minute status to Closed if ToDo is closed or deleted"""
+	//Update minute status to Closed if ToDo is closed or deleted
 	if doc.reference_type != "Meeting" or doc.flags.from_meeting:
 		return
 
@@ -189,7 +190,7 @@ def update_minute_status(doc, method=None):
 				minute.db_set("todo", None, update_modified=False)
 				minute.db_set("status", "Closed", update_modified=False)
 				
-
+"""
 """
 @frappe.whitelist()
 def send_minutes(meeting):
